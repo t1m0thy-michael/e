f1833235343bf8c11ea2b27603fb28f2b6eec2c1
Timothy Michael <tim.everington@gmail.com>
2020-04-05 20:47:24 +0100

    removed browser specific default events

--- src/index.js
+++ src/index.js
@@ -1,11 +1,7 @@
 import { sub } from './sub'
 import { pub } from './pub'
 import { remove } from './remove'
-import { stdEvt } from './stdEvt'
 
-export const create = () => Object.create(Event, {
-	topics: { value: [], writable: false }
-})
 
 const Event = {
 	pub: pub,
@@ -14,11 +10,14 @@ const Event = {
 	toString: () => '[object Eventbus]',
 }
 
+export const create = () => Object.create(Event, {
+	topics: { value: [], writable: false }
+})
+
 export const event = (() => {
 	const gbl = globalThis || window || self || global
 	if (!gbl.e) {
 		gbl.e = create()
-		stdEvt(gbl.e)
 	}
 	return gbl.e
 })()
